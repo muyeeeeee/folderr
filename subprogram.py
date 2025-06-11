3a872146c3ae42497305cc54116edfa1aa4c2e09
muyeeeeee <muye@api-school.com>
2025-06-11 08:30:02 +0700

    Create subprogram.py

---
+++ subprogram.py
@@ -0,0 +1,27 @@
+#my sub program
+def Mony():
+    print("She lives in Siem Reap")
+    print("She likes watching movie")
+    print("She is so funny")
+    print("She likes to travel")
+Mony()
+
+#create a sub program function
+def average(x, y):
+    return (x + y) / 2
+average (5,5)
+print(average(5,5))
+print(average(10,10))
+
+def flowers():
+    print("I like white roses")
+    print("Also lilies")
+    print("Orchids are also beautiful")
+    print("Lily of the valley is really unique!")
+flowers()
+
+#subprogramfunction
+def area(width, height):
+    area = width * height
+    return area
+print(area(10,2))

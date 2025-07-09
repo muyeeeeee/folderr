0bf6c3fb10c9740e1636da4f596b7e0935d6e136
muyeeeeee <muye@api-school.com>
2025-07-09 08:45:03 +0700

    Create flowchartfood.py

---
+++ flowchartfood.py
@@ -0,0 +1,14 @@
+question = input("Are you hungry?")
+if question == "no":
+    print = ("Drink water")
+else:
+    answer = input("Do you want sweet or savory?")
+  
+if question == "sweet":
+    print("eat cookies, ice cream or fruits")
+else:
+    savory = input("Do you prefer light or heavy?")
+if savory == "light":
+    print("Salad, soup or chips")
+else:
+    print("Burger, noodles, pasta")
